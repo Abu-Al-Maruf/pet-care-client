bc910d0b3cc5cb71a750dabcdc8a80f39a8c0607
Abu Al Maruf <mdmaruf55667711@gmail.com>
2024-07-12 12:09:32 +0600

    navbar responsive menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,56 @@
 import { Link, NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { Squash as Hamburger } from "hamburger-react";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setOpen] = useState(false);
   const navLink = (
     <>
-      <NavLink to={"/"} className="text-lg font-semibold hover:text-blue-700">
+      <NavLink
+        to={"/"}
+        className={({ isActive }) =>
+          isActive
+            ? "text-lg font-semibold py-1 px-4 rounded bg-orange-400 text-white"
+            : "text-lg font-semibold py-1 px-4 rounded hover:bg-orange-400 hover:text-white"
+        }
+      >
         Home
       </NavLink>
-      <NavLink to={"/service"} className="text-lg font-semibold hover:text-blue-700">
+      <NavLink
+        to={"/service"}
+        className={({ isActive }) =>
+          isActive
+            ? "text-lg font-semibold py-1 px-4 rounded bg-orange-400 text-white"
+            : "text-lg font-semibold py-1 px-4 rounded hover:bg-orange-400 hover:text-white"
+        }
+      >
         Service
       </NavLink>
-      <NavLink to={"/about"} className="text-lg font-semibold hover:text-blue-700">
+      <NavLink
+        to={"/about"}
+        className={({ isActive }) =>
+          isActive
+            ? "text-lg font-semibold py-1 px-4 rounded bg-orange-400 text-white"
+            : "text-lg font-semibold py-1 px-4 rounded hover:bg-orange-400 hover:text-white"
+        }
+      >
         About
       </NavLink>
-      <NavLink to={"/login"} className="text-lg font-semibold hover:text-blue-700">
+      <NavLink
+        to={"/login"}
+        className={({ isActive }) =>
+          isActive
+            ? "text-lg font-semibold py-1 px-4 rounded bg-orange-400 text-white"
+            : "text-lg font-semibold py-1 px-4 rounded hover:bg-orange-400 hover:text-white"
+        }
+      >
         login
       </NavLink>
     </>
   );
   return (
-    <div className="flex items-center justify-between p-4 bg-gray-200">
+    <div className="flex items-center justify-between p-4 bg-gray-200 shadow-lg">
       <Link
         to={"/"}
         className="text-2xl font-semibold flex flex-col items-center"
@@ -27,8 +58,12 @@ const Navbar = () => {
         <img className="w-16" src={logo} alt="" />
       </Link>
       <div>
-        <nav className="flex gap-6 mr-5">{navLink}</nav>
+        <nav className="hidden sm:flex gap-6 mr-5 ">{navLink}</nav>
+        <span className="sm:hidden flex">
+          <Hamburger toggled={isOpen} toggle={setOpen} />
+        </span>
       </div>
+      {isOpen && <nav className="flex md:hidden flex-col fixed top-0 left-0  h-screen p-10 pr-20 transition-all bg-blue-400">{navLink}</nav>}
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,26 @@
-
 const Home = () => {
-    return (
-        <div>
-            Home..
-        </div>
-    );
+  return (
+    <div className="flex flex-col-reverse gap-5 md:flex-row items-center gap-x-10 justify-between py-20 px-10 bg-orange-100/50">
+      <div className="flex-1 space-y-7">
+        <h1 className="text-3xl sm:text-5xl font-extrabold text-orange-500">
+          Get Your Organic Pet Food
+        </h1>
+        <p className="text-xl font-light">
+          Your Pet deserves a best quality food and healthy snacks. So, we have
+          the best pet food to offer with affordable price.
+        </p>
+        <button className="btn bg-orange-500 text-white  hover:text-orange-500">
+          SHOP NOW
+        </button>
+      </div>
+      <div className="flex-1">
+        <img
+          src="https://www.sgs.com/-/media/sgscorp/images/health-and-nutrition/dog-food-in-bowl-and-dog-biscuits.cdn.en-BD.1.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
